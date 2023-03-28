6011b4329739d3587ba949b2a72afe70969a1370
luigiolivi <eterowiczluigi@gmail.com>
2023-03-27 21:35:44 -0300

    incrementação para a quantidade de vezes que se repete o determinado número

--- math.js
+++ math.js
@@ -1,97 +1,224 @@
 // DIGITE OS NÚMEROS AQUI, COM VÍRGULA ENTRE ELES.
 
 const numeros = [
-    
-    2,7,3
 
-] 
+    1,2,3
+
+]
+
+// DIGITE QUANTAS VEZES SE REPETE OS NÚMEROS DETERMINADOS ACIMA.
+
+const quantidade = [
+
+    3,3,3
+
+]
+
+const numerosRepeticao = []
 
 const tamanhoArray = numeros.length
+const tamanhoArrayNumerosRepeticao = quantidade.length
 
-const media = () => {
-    let soma = 0
+let repete = false
 
-    for (i = 0; i < tamanhoArray; i++) {
-        soma += numeros[i]
+for (let i = 0; i < tamanhoArrayNumerosRepeticao; i++) {
+    if (quantidade[i] > 1) {
+        repete = true
+
+        break
+    }
+}
+
+if (repete === true) {
+    let somador = 0
+
+    for (let i = 0; i < tamanhoArrayNumerosRepeticao; i++) {
+        somador += quantidade[i]
     }
 
-    let mediaFinal = soma / tamanhoArray
+    let comparador = 0
 
-    return mediaFinal
+    for (let i = 0; i < somador; i++) {
+        for (let j = 0; j < quantidade[i]; j++) {
+            numerosRepeticao[comparador] = numeros[i]
+
+            comparador++
+        }
+    }
+}
+
+const tamanhoArrayNovo = numerosRepeticao.length
+
+
+const media = () => {
+    if (repete === false) {
+        let soma = 0
+
+        for (i = 0; i < tamanhoArray; i++) {
+            soma += numeros[i]
+        }
+
+        let mediaFinal = soma / tamanhoArray
+
+        return mediaFinal
+    }
+
+    else {
+        let soma = 0
+
+        for (i = 0; i < tamanhoArrayNovo; i++) {
+            soma += numerosRepeticao[i]
+        }
+
+        let mediaFinal = soma / tamanhoArrayNovo
+
+        return mediaFinal
+    }
+
 }
 
 console.log("Média aritmética: " + media())
 
 
 const mediana = () => {
-	  const ordenado = numeros.sort(function(a, b) {
-  return a - b;
-})
+    if (repete === false) {
+        const ordenado = numeros.sort(function (a, b) {
+            return a - b;
+        })
 
-    if (tamanhoArray % 2 === 0) {
-    	const numeroMeio1 = (tamanhoArray / 2) - 1
-    	const numeroMeio2 = numeroMeio1 + 1
-    	const mediana = (ordenado[numeroMeio1] + ordenado[numeroMeio2]) / 2
-    	
-    	return mediana
+        if (tamanhoArray % 2 === 0) {
+            const numeroMeio1 = (tamanhoArray / 2) - 1
+            const numeroMeio2 = numeroMeio1 + 1
+            const mediana = (ordenado[numeroMeio1] + ordenado[numeroMeio2]) / 2
+
+            return mediana
+        }
+
+        else {
+            const numeroMeio = (tamanhoArray / 2) - 1
+            const numeroMeioArredondado = Math.ceil(numeroMeio)
+
+            const mediana = ordenado[numeroMeioArredondado]
+
+            return mediana
+        }
     }
-    
+
     else {
-    	const numeroMeio = (tamanhoArray / 2) - 1
-    	const numeroMeioArredondado = Math.ceil(numeroMeio)
-    	
-    	const mediana = ordenado[numeroMeioArredondado]
-    	
-    	return mediana
+        const ordenado = numerosRepeticao.sort(function (a, b) {
+            return a - b;
+        })
+
+        if (tamanhoArrayNovo % 2 === 0) {
+            const numeroMeio1 = (tamanhoArrayNovo / 2) - 1
+            const numeroMeio2 = numeroMeio1 + 1
+            const mediana = (ordenado[numeroMeio1] + ordenado[numeroMeio2]) / 2
+
+            return mediana
+        }
+
+        else {
+            const numeroMeio = (tamanhoArrayNovo / 2) - 1
+            const numeroMeioArredondado = Math.ceil(numeroMeio)
+
+            const mediana = ordenado[numeroMeioArredondado]
+
+            return mediana
+        }
     }
 }
 
 console.log("Mediana: " + mediana())
 
 
-const varianca = () => {
-    let soma = 0
+const variancia = () => {
+    if (repete === false) {
+        let soma = 0
 
-    for (let i = 0; i < tamanhoArray; i++) {
-        soma += (numeros[i] - media()) ** 2
+        for (let i = 0; i < tamanhoArray; i++) {
+            soma += (numeros[i] - media()) ** 2
+        }
+
+        const variancia = soma / tamanhoArray
+
+        return variancia
     }
 
-    const varianca = soma / tamanhoArray
+    else {
+        let soma = 0
 
-    return varianca
-}   
+        for (let i = 0; i < tamanhoArrayNovo; i++) {
+            soma += (numerosRepeticao[i] - media()) ** 2
+        }
 
-console.log("Variança: " + varianca())
+        const variancia = soma / tamanhoArrayNovo
+
+        return variancia
+    }
+
+}
+
+console.log("Variância: " + variancia())
 
 
-const desvioPadrao = Math.sqrt(varianca())
+const desvioPadrao = Math.sqrt(variancia())
 
 console.log("Desvio-padrão: " + desvioPadrao)
 
 
 const moda = () => {
-    const getOccurrences = (array, value) => array.reduce((acc, item) => value === item ? acc + 1 : acc, 0)
+    if (repete === false) {
 
-    const quantidadeNumerosRepetidos = []
+        const getOccurrences = (array, value) => array.reduce((acc, item) => value === item ? acc + 1 : acc, 0)
 
-    for (i = 0; i < 1000; i++) {
-        quantidadeNumerosRepetidos[i] = getOccurrences(numeros, i)
-    }
+        const quantidadeNumerosRepetidos = []
 
-    const maiorNumero = Math.max(...quantidadeNumerosRepetidos)
+        for (i = 0; i < 1000; i++) {
+            quantidadeNumerosRepetidos[i] = getOccurrences(numeros, i)
+        }
 
-    const indexQuantidadeNumerosRepetidos = quantidadeNumerosRepetidos.findIndex(index => index === maiorNumero)
+        const maiorNumero = Math.max(...quantidadeNumerosRepetidos)
 
-    const maioresNumerosRepetidosFiltrados = quantidadeNumerosRepetidos.filter(number => number === maiorNumero)
+        const indexQuantidadeNumerosRepetidos = quantidadeNumerosRepetidos.findIndex(index => index === maiorNumero)
 
-    if (maioresNumerosRepetidosFiltrados.length === 1) {
-        const moda = indexQuantidadeNumerosRepetidos
+        const maioresNumerosRepetidosFiltrados = quantidadeNumerosRepetidos.filter(number => number === maiorNumero)
 
-        return moda
+        if (maioresNumerosRepetidosFiltrados.length === 1) {
+            const moda = indexQuantidadeNumerosRepetidos
+
+            return moda
+        }
+
+        else {
+            return -1
+        }
     }
 
     else {
-        return -1
+
+        const getOccurrences = (array, value) => array.reduce((acc, item) => value === item ? acc + 1 : acc, 0)
+
+        const quantidadeNumerosRepetidos = []
+
+        for (i = 0; i < 1000; i++) {
+            quantidadeNumerosRepetidos[i] = getOccurrences(numerosRepeticao, i)
+        }
+
+        const maiorNumero = Math.max(...quantidadeNumerosRepetidos)
+
+        const indexQuantidadeNumerosRepetidos = quantidadeNumerosRepetidos.findIndex(index => index === maiorNumero)
+
+        const maioresNumerosRepetidosFiltrados = quantidadeNumerosRepetidos.filter(number => number === maiorNumero)
+
+        if (maioresNumerosRepetidosFiltrados.length === 1) {
+            const moda = indexQuantidadeNumerosRepetidos
+
+            return moda
+        }
+
+        else {
+            return -1
+        }
     }
 }
 
@@ -102,4 +229,5 @@ if (moda() === -1) {
 else {
     console.log("Moda: " + moda())
 }
+
 
